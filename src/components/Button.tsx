ed4759a5aa3f405b9d6d6cd008463529de076970
taynara veloso <taynarasantos2.3@hotmail.com>
2024-06-13 00:34:07 -0300

    :star: styled-components TypeScript - Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,13 @@
-import styles from './Button.module.css'
+import { ButtonContainer, ButtonVariant } from './Button.styles';
 
 interface ButtonProps {
-  color?: 'primary' | 'secondary' | 'warning' | 'success';
+  variant?: ButtonVariant;
 }
 
-export function Button({ color = 'primary' }: ButtonProps){
+export function Button({ variant = 'primary' }: ButtonProps){
   return (
     <>
-      <button className={`${styles.button} ${styles[color]}`}>Enviar</button>
+      <ButtonContainer variant={variant}>Enviar</ButtonContainer>
     </>
 )
   
