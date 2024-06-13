c14ec377052a3afde3762ebcead812e91f93e1fe
taynara veloso <taynarasantos2.3@hotmail.com>
2024-06-12 23:29:24 -0300

    :tada: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from './components/Button'
+import './App.css'
+
+export function App() {
+
+  return (
+    <>
+      <Button color="primary"/>
+      <Button color="secondary"/>
+      <Button color="success"/>
+      <Button color="warning"/>
+      <Button />
+    </>
+  )
+}
+
