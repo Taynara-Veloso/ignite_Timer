e01d940cbcbe8fd180043ac0a48698a434c119f2
taynara veloso <taynarasantos2.3@hotmail.com>
2024-06-24 19:45:44 -0300

    :star: applying context: newCycleForm page

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,40 @@
+import { useContext } from 'react'
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+import { CyclesContext } from '../..'
+import { useFormContext } from 'react-hook-form'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+      <datalist id="task-suggestions">
+        <option value="Explorer - Rocketseat" />
+        <option value="React ignite - Rocketseat" />
+        <option value="Node - Rocketseat" />
+        <option value="Projeto - Beauty" />
+      </datalist>
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="- 00 +"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
